6ea7a571b5d89c7309a21cfffb7e3b93b62ab22d
Danny Beckett <k1w1onoe@hotmail.com>
2018-04-30 13:14:09 +1200

    Add project files.

---
+++ TheShortWayASPCore2ToDo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheShortWayASPCore2ToDo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
